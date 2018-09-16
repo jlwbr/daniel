e3265188bcaf57b0b97da8e298ecc132bc598450
PatMiks <34721148+PatMiks@users.noreply.github.com>
2018-09-16 21:14:31 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -2,8 +2,8 @@ const logger = require('winston');
 
 module.exports = {
     name: 'ban',
-    description: 'Ban een speler.',
-    usage: '[naam speler]',
+    description: 'Ban een gebruiker.',
+    usage: '[naam gebruiker]',
     permission: [ 'KICK_MEMBERS' ],
     guildOnly: true,
     execute(message) {
@@ -11,19 +11,19 @@ module.exports = {
         if (user) {
           const member = message.guild.member(user);
           if (member) {
-            member.ban({ reason: 'Wees niet stout' }).then(() => {
+            member.ban({ reason: 'Lief doen, hè!' }).then(() => {
               message.reply(`${user.tag} is succesvol geband`);
             }).catch(err => {
-              message.reply('ik kon deze persoon niet bannen');
+              message.reply('ik kon deze persoon niet bannen!');
               logger.error(err);
             });
           }
           else {
-            message.reply('deze gebruiker is niet in de server!');
+            message.reply('deze gebruiker zit niet in de server!');
           }
         }
         else {
-          message.reply('je moet een gebruiker toevoegen om te bannen!');
+          message.reply('je moet een naam toevoegen om te bannen!');
         }
     },
 };
